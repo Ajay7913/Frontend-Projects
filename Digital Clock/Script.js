d69b34787460aa4f9a4a1d8265461b70042cace4
Ajay7913 <ajayware79@gmail.com>
2023-10-21 11:19:09 +0530

    Ajay7913/Frontend-Projects

---
+++ Digital Clock/Script.js
@@ -0,0 +1,19 @@
+const hr = document.querySelector("#hr");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+const ampm = document.querySelector("#ampm");
+
+const update = () => {
+  let date = new Date();
+  let h = date.getHours();
+  if (h >= 12) {
+    ampm.innerText = "PM";
+    h = h != 12 && h != 24 ? h % 12 : 12;
+  } else ampm.innerText = "AM";
+
+  hr.innerText = `0${h}`.slice(-2);
+  min.innerText = `0${date.getMinutes()}`.slice(-2);
+  sec.innerText = `0${date.getSeconds()}`.slice(-2);
+};
+setInterval(update, 1000);
+window.onload = update();
